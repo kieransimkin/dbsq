479f8a96098f3b799d8d9f4465a09879850cde4b
Kieran Simkin <kieran.simkin@public-i.info>
2012-02-09 18:02:30 +0000

    some work

--- dbsq.class.php
+++ dbsq.class.php
@@ -4,6 +4,10 @@ require_once 'DB.php';
 class DBSQ {
 	static private $_dsn=null;
 	static private $_db=null;
+	static private $_lazyLoad='row';
+	private $_lazyLoadId=null;
+	private $_lazyLoadIndexName=null;
+	private $_lazyLoadMode=null;
 	private $_data=array();
 	static function setMySQLCredentials($username,$password,$database,$host='localhost') { 
 		self::$_dsn="mysql://$username:$password@$host/$database";
@@ -13,13 +17,36 @@ class DBSQ {
 		self::$_dsn="mysqli://$username:$password@$host/$database";
 		self::$_db=DB::connect(self::$_dsn);
 	}
-	public static function get($id,$uniqueindexname='id') { 
-		$res=self::$_db->getRow('select * from `'.get_called_class().'` WHERE ? = ?', array($uniqueindexname, $id),DB_FETCHMODE_ASSOC);
+	static private function _getNewInstance() { 
 		$classname=get_called_class();
-		$new = new $classname;
-		$new->_loadData($res);
+		$new=new $classname;
+		return $new;
 	}
-	public static function getAll($where="1 = 1",$args=array()) { 
+	static public function get($id,$uniqueindexname='id',$forcelazy=false,$forceprecache=false) { 
+		if ($forcelazy && $forcelazy!='row' && $forcelazy!='col') { 
+			throw new Exception('forcelazy must be row or col');
+			return;
+		}
+		$new=self::_getNewInstance();
+		$new->_lazyLoadId=$id;
+		$new->_lazyLoadIndexName=$uniqueindexname;
+		if ((self::$_lazyLoad || $forcelazy) && !$forceprecache) { 
+			if ($forcelazy) { 
+				$new->_lazyLoadMode=$forcelazy;
+			} else { 
+				$new->_lazyLoadMode=self::$_lazyLoad;
+			}
+			return $new;
+		}
+		$new->_doGetRow($id,$uniqueindexname
+	}
+
+	private function _doGetRow() { 
+		$res=self::$_db->getRow('select * from `'.get_called_class().'` WHERE ? = ?', array($this->_lazyLoadIndexName, $this->_lazyLoadId),DB_FETCHMODE_ASSOC);
+		$this->_loadDataRow($res);
+	}
+	private function _doGetCol($
+	static public function getAll($where="1 = 1",$args=array()) { 
 		$res=self::$_db->query('select * from `'.get_called_class().'` WHERE '.$where, $args);
 	}
 	
